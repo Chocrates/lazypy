d06c3a6cd8d65fe621fe86256c1d7a25acdb5f97
Chris McIntosh <chris_mcintosh@bridge360.com>
2016-10-02 18:09:04 -0500

    Code for tutorial 4

--- sdl_hello.py
+++ sdl_hello.py
@@ -1,6 +1,8 @@
 import sys
+from enum import Enum
 import ctypes
 from sdl2 import *
+
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
@@ -10,20 +12,22 @@ screen_surface = None
 event = SDL_Event()
 quit = False
 
+class key_press_surfaces(Enum):
+    KEY_PRESS_SURFACE_DEFAULT = 0
+    KEY_PRESS_SURFACE_UP = 1
+    KEY_PRESS_SURFACE_DOWN = 2
+    KEY_PRESS_SURFACE_LEFT = 3
+    KEY_PRESS_SURFACE_RIGHT = 4
+    KEY_PRESS_SURFACE_TOTAL = 5
+
 key_surfaces = {
     key_press_surfaces.KEY_PRESS_SURFACE_DEFAULT: None,
     key_press_surfaces.KEY_PRESS_SURFACE_UP: None,
     key_press_surfaces.KEY_PRESS_SURFACE_DOWN: None,
     key_press_surfaces.KEY_PRESS_SURFACE_LEFT: None,
+    key_press_surfaces.KEY_PRESS_SURFACE_RIGHT: None,
     key_press_surfaces.KEY_PRESS_SURFACE_TOTAL: None
 }
-
-class key_press_surfaces(enum):
-    KEY_PRESS_SURFACE_DEFAULT = 0
-    KEY_PRESS_SURFACE_UP = 1
-    KEY_PRESS_SURFACE_DOWN = 2
-    KEY_PRESS_SURFACE_LEFT = 3
-    KEY_PRESS_SURFACE_TOTAL =4
 
 def init():
     success = True
@@ -47,28 +51,28 @@ def load_media():
     success = True
     global key_surfaces
 
-    key_surfaces[KEY_PRESS_SURFACE_DEFAULT] = load_surface(b'press.bmp')
-    if key_surfaces[KEY_PRESS_SURFACE_DEFAULT] is None:
+    key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_DEFAULT] = load_surface(b'press.bmp')
+    if key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_DEFAULT] is None:
         print('Unable to load default image %s.  SDL_Error: %s' % ('press.bmp',SDL_GetError()))
         success = False
 
-    key_surfaces[KEY_PRESS_SURFACE_UP] = load_surface(b'up.bmp')
-    if key_surfaces[KEY_PRESS_SURFACE_UP] is None:
+    key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_UP] = load_surface(b'up.bmp')
+    if key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_UP] is None:
         print('Unable to load default image %s.  SDL_Error: %s' % ('up.bmp',SDL_GetError()))
         success = False
     
-    key_surfaces[KEY_PRESS_SURFACE_DOWN] = load_surface(b'down.bmp')
-    if key_surfaces[KEY_PRESS_SURFACE_DOWN] is None:
+    key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_DOWN] = load_surface(b'down.bmp')
+    if key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_DOWN] is None:
         print('Unable to load default image %s.  SDL_Error: %s' % ('down.bmp',SDL_GetError()))
         success = False
 
-    key_surfaces[KEY_PRESS_SURFACE_LEFT] = load_surface(b'left.bmp')
-    if key_surfaces[KEY_PRESS_SURFACE_LEFT] is None:
+    key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_LEFT] = load_surface(b'left.bmp')
+    if key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_LEFT] is None:
         print('Unable to load default image %s.  SDL_Error: %s' % ('left.bmp',SDL_GetError()))
         success = False
 
-    key_surfaces[KEY_PRESS_SURFACE_RIGHT] = load_surface(b'right.bmp')
-    if key_surfaces[KEY_PRESS_SURFACE_RIGHT] is None:
+    key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_RIGHT] = load_surface(b'right.bmp')
+    if key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_RIGHT] is None:
         print('Unable to load default image %s.  SDL_Error: %s' % ('right.bmp',SDL_GetError()))
         success = False
 
@@ -102,21 +106,22 @@ def main():
         if not load_media():
             print('Failed to load media')
         else:
+            current_surface = key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_DEFAULT]
             while not quit:
                 while SDL_PollEvent(ctypes.byref(event)) != 0:
                     if event.type == SDL_QUIT:
                         quit = True
                     elif event.type == SDL_KEYDOWN:
-                        if event.key.keysym.sym == SDKLK_UP:
-                            current_surface = key_surface[KEY_PRESS_SURFACE_UP]
-                        elif event.key.keysym.sym == SDKLK_DOWN:
-                            current_surface = key_surface[KEY_PRESS_SURFACE_DOWN]
-                        elif event.key.keysym.sym == SDKLK_LEFT:
-                            current_surface = key_surface[KEY_PRESS_SURFACE_LEFT]
-                        elif event.key.keysym.sym == SDKLK_RIGHT:
-                            current_surface = key_surface[KEY_PRESS_SURFACE_RIGHT]
+                        if event.key.keysym.sym == SDLK_UP:
+                            current_surface = key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_UP]
+                        elif event.key.keysym.sym == SDLK_DOWN:
+                            current_surface = key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_DOWN]
+                        elif event.key.keysym.sym == SDLK_LEFT:
+                            current_surface = key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_LEFT]
+                        elif event.key.keysym.sym == SDLK_RIGHT:
+                            current_surface = key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_RIGHT]
                         else:
-                            current_surface = key_surface[KEY_PRESS_SURFACE_DEFAULT]
+                            current_surface = key_surfaces[key_press_surfaces.KEY_PRESS_SURFACE_DEFAULT]
                         
                 SDL_BlitSurface(current_surface, None, screen_surface, None)
                 SDL_UpdateWindowSurface(window)
